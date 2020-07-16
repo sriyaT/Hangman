18e7a67d1981d0a63180ccc63de31c1b5b18f6d2
Sriya <tsriya6@gmail.com>
2020-07-17 02:14:41 +0530

    updated with responsiveness/17-07-2020/

--- script.js
+++ script.js
@@ -4,18 +4,30 @@ const playAgainBtn = document.getElementById('play-button');
 const popup = document.getElementById('popup-container');
 const notification = document.getElementById('notification-container');
 const finalMessage = document.getElementById('final-message');
+const spanInput = document.getElementById('span-input');
 
 const figureParts = document.querySelectorAll('.figure-part');
 let words = [];
 let selectedWord = '';
+let gameOver = false;
 
 fetch('https://random-word-api.herokuapp.com/word?number=10')
   .then((res) => res.json())
   .then((data) => {
     words = data;
+    selectedWord = words[Math.floor(Math.random() * words.length)];
+    displayWord();
   });
+
+// selectedWord = words[Math.floor(Math.random() * words.length)];
+
 const correctLetters = [];
 const wrongLetters = [];
+
+//Opens the keyboard onclick of span for mobile user
+function openKeyboard() {
+  spanInput.focus();
+}
 
 //Show Hidden Word
 function displayWord() {
@@ -24,7 +36,7 @@ function displayWord() {
       .split('')
       .map(
         (letter) => `
-        <span class = "letter">
+        <span class = "letter" onclick = "openKeyboard()" >
         ${correctLetters.includes(letter) ? letter : ''}
         </span>
         `
@@ -33,9 +45,10 @@ function displayWord() {
 `;
   const innerWord = wordEl.innerText.replace(/\n/g, '');
 
-  if (innerWord == selectedWord) {
+  if (innerWord.length > 0 && innerWord == selectedWord) {
     finalMessage.innerText = 'congratulations! You Won! 🥳';
     popup.style.display = 'flex';
+    gameOver = true;
   }
 }
 
@@ -61,8 +74,15 @@ function updateWrongLettersEl() {
 
   // check if lost
   if (wrongLetters.length == figureParts.length) {
-    finalMessage.innerText = 'Unfortunately You Lost. ☹️';
+    finalMessage.innerText = `Unfortunately You Lost. ☹️ \n Word is `;
     popup.style.display = 'flex';
+
+    let lastChild = document.createElement('span');
+    lastChild.innerText = `${selectedWord}`;
+    lastChild.style.color = 'ghostwhite';
+    lastChild.style.fontSize = '1.3em';
+    finalMessage.appendChild(lastChild);
+    gameOver = true;
   }
 }
 
@@ -76,9 +96,21 @@ function ShowNotification() {
 }
 
 //  Keydown letter press
-window.addEventListener('keydown', (e) => {
-  if (e.keyCode >= 65 && e.keyCode <= 90) {
-    const letter = e.key;
+window.addEventListener('keyup', (e) => {
+  const charString = spanInput.value;
+  let lastChar = e.key.length === 1 ? e.key : charString[charString.length - 1];
+  // charString.length > 0 ? charString[charString.length - 1] : e.key;
+  lastChar = lastChar.toUpperCase();
+  const finalCode = lastChar.charCodeAt(0);
+
+  //   const finalCode = +code ? code : e.keyCode;
+
+  console.log('keycode', finalCode, lastChar);
+
+  if (!gameOver && finalCode >= 65 && finalCode <= 90) {
+    const letter = lastChar;
+
+    selectedWord = selectedWord.toUpperCase();
 
     if (selectedWord.includes(letter)) {
       if (!correctLetters.includes(letter)) {
@@ -102,15 +134,16 @@ window.addEventListener('keydown', (e) => {
 
 // restart game and play again
 playAgainBtn.addEventListener('click', () => {
+  spanInput.value = '';
+  gameOver = false;
   //Empty arrays
   correctLetters.splice(0);
   wrongLetters.splice(0);
 
   selectedWord = words[Math.floor(Math.random() * words.length)];
 
-  displayWord();
-
   updateWrongLettersEl();
+  displayWord();
 
   popup.style.display = 'none';
 });
